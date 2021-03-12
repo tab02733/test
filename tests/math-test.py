459852384939529b6acedc9c26f698ec796071cd
tab02733 <akihiro.yamamoto@topgate.co.jp>
2021-03-12 16:47:40 +0900

    訂正

--- tests/math-test.py
+++ tests/math-test.py
@@ -4,5 +4,5 @@ from src.math import addition
 class MathTest(unittest.TestCase):
     def test_addition(self):
         actual = addition(3,4)
-        expected = 8 # 実行結果と一致
+        expected = 7 # 実行結果と一致
         self.assertEqual(actual, expected)
